9924e030944d9aa8e06192c24bd34c5f8b24d76a
seaman <semen.bko@ya.ru>
2015-02-12 23:45:20 +0300

    styl compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
 var gulp = require('gulp');
+
+// reload
+var connect = require('gulp-connect');
+
+// js
 var react = require('gulp-react');
 var browserify = require('browserify');
-var connect = require('gulp-connect');
 var clean = require('gulp-clean');
 var source = require('vinyl-source-stream');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+
+// Style
+var concat = require('gulp-concat-css');
+var stylus = require('gulp-stylus');
+var minify = require('gulp-minify-css');
 
 gulp.task('connect', function(){
 	connect.server({
@@ -41,10 +50,19 @@ gulp.task('html', function(){
 		.pipe(connect.reload());
 });
 
+gulp.task('style', function(){
+	gulp.src('./app/styles/styl/**/*.styl')
+		.pipe(stylus())
+		.pipe(concat('main.css'))
+		.pipe(minify())
+		.pipe(gulp.dest('./app/styles'))
+		.pipe(connect.reload());
+});
 
-gulp.task('watch', ['react', 'browserify'], function(){
+gulp.task('watch', ['react', 'browserify', 'style'], function(){
 	gulp.watch('./app/js/jsx/**/**/*.jsx', ['react', 'browserify']);
 	gulp.watch('./app/*.html', ['html']);
+	gulp.watch('./app/styles/styl/**/*.styl', ['style']);
 });
 
 gulp.task('default', ['connect', 'watch']);
