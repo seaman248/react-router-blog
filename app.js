28c30d4b1ba5e0f81711022ddff057b8903737d1
seaman <semen.bko@ya.ru>
2015-02-20 19:35:05 +0300

    index router require

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // ROUTERS
+app.use('/', require('./routers/mainPage'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
