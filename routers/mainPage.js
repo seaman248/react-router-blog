ae3c262a84f803914df7aad248730f77ad2cc2f9
seaman <semen.bko@ya.ru>
2015-02-20 21:22:20 +0300

    findpost routers

--- routers/mainPage.js
+++ routers/mainPage.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var Post = require('../models').Post;
 
 router.get('/', function(req, res){
 	res.render('index');
 });
+
+router.get('/posts', function(req, res){
+	Post.find(function(err, posts){
+		res.json(posts);
+	});
+});
+
+router.get('/post/:id', function(req, res){
+	Post.findOne({
+		'pathId': req.params.id
+	}, function(err, post){
+		res.json(post);
+	});
+
+})
 
 module.exports = router;
