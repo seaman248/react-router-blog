1b6b273dde3a098c8c7485d71400dfe58c08d90a
seaman <semen.bko@ya.ru>
2015-02-20 19:15:37 +0300

    com

--- models/index.js
+++ models/index.js
@@ -14,17 +14,17 @@ var postSchema = new mongoose.Schema({
 
 var Post = module.exports.Post = mongoose.model('Post', postSchema);
 
-// test db init
-for (var i=0, max=100; i<max; i++){
-	(function(e){
-		var post = new Post({
-			pathId: 'testPost_id_'+e,
-			title: 'Page title Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, ipsum '+e,
-			body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique praesentium dolores unde temporibus nam, suscipit odio optio possimus. Vel dolore, mollitia cupiditate laborum ut totam expedita accusamus deleniti praesentium impedit.',
-			tags: ['tag1', 'tag2', 'tag3'],
-		});
-		post.save(function(err, post){
-			console.log(post.pathId);
-		});
-	})(i);
-}
+// // test db init
+// for (var i=0, max=100; i<max; i++){
+// 	(function(e){
+// 		var post = new Post({
+// 			pathId: 'testPost_id_'+e,
+// 			title: 'Page title Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, ipsum '+e,
+// 			body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique praesentium dolores unde temporibus nam, suscipit odio optio possimus. Vel dolore, mollitia cupiditate laborum ut totam expedita accusamus deleniti praesentium impedit.',
+// 			tags: ['tag1', 'tag2', 'tag3'],
+// 		});
+// 		post.save(function(err, post){
+// 			console.log(post.pathId);
+// 		});
+// 	})(i);
+// }
